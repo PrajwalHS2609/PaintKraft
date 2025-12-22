0a763484a76a4a16a107b1607626b09906eeaeee
Prajwal H S <prajwalhs4india@gmail.com>
2025-12-22 15:14:42 +0530

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import { Montserrat } from "next/font/google";
-import "./globals.css"
-import LpPaintNavbar from './../components/PaintLandingPage/LpPaintNavbar/LpPaintNavbar';
-import LpPaintFooter from './../components/PaintLandingPage/LpPaintFooter/LpPaintFooter';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
+import "./globals.css";
+import LpPaintNavbar from "./../components/PaintLandingPage/LpPaintNavbar/LpPaintNavbar";
+import LpPaintFooter from "./../components/PaintLandingPage/LpPaintFooter/LpPaintFooter";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -19,11 +17,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-
+      <head>
+        <link rel="icon" type="image/png" href="./favicon.png" />
+      </head>
       <body className={montserrat.className}>
-        <LpPaintNavbar/>
+        <LpPaintNavbar />
         {children}
-        <LpPaintFooter/>
+        <LpPaintFooter />
       </body>
     </html>
   );
