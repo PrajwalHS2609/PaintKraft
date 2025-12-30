d8988c2917e841a624799478347d9d3ea2a48351
Prajwal H S <prajwalhs4india@gmail.com>
2025-12-30 11:32:55 +0530

    google code2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,80 +1,58 @@
 import { Montserrat } from "next/font/google";
 import "./globals.css";
-import LpPaintNavbar from "./../components/PaintLandingPage/LpPaintNavbar/LpPaintNavbar";
-import LpPaintFooter from "./../components/PaintLandingPage/LpPaintFooter/LpPaintFooter";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Script from "next/script";
+
+import LpPaintNavbar from "../components/PaintLandingPage/LpPaintNavbar/LpPaintNavbar";
+import LpPaintFooter from "../components/PaintLandingPage/LpPaintFooter/LpPaintFooter";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
   weight: ["300", "400", "500"],
-  // variable: "--font-primary",
 });
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" type="image/png" href="./favicon.png" />
-        <Script
-          id="gtag-conversion"
-          strategy="afterInteractive"
+        {/* Favicon */}
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        {/* ================= GOOGLE ADS BASE TAG (REQUIRED) ================= */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17441114317"
+        ></script>
+
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-      function gtag_report_conversion(url) {
-        var callback = function () {
-          if (typeof(url) != 'undefined') {
-            window.location = url;
-          }
-        };
-        gtag('event', 'conversion', {
-          'send_to': 'AW-17441114317/vVPECIDiwc4bEM2RyfxA',
-          'event_callback': callback
-        });
-        return false;
-      }
-    `,
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17441114317');
+            `,
           }}
-        />
-        {/* <!-- Google Tag Manager --> */}
-        {/* Google Ads Conversion */}
-        <Script
-          id="gtag-conversion"
-          strategy="afterInteractive"
+        ></script>
+
+        {/* ========== GOOGLE ADS CONVERSION FUNCTION ========== */}
+        <script
           dangerouslySetInnerHTML={{
             __html: `
-              function gtag_report_conversion(url) {
-                var callback = function () {
-                  if (typeof url !== 'undefined') {
-                    window.location = url;
-                  }
-                };
+              function reportConversion() {
                 gtag('event', 'conversion', {
-                  'send_to': 'AW-17441114317/vVPECIDiwc4bEM2RyfxA',
-                  'event_callback': callback
+                  'send_to': 'AW-17441114317/niVMCNvywc4bEM2RyfxA'
                 });
-                return false;
               }
             `,
           }}
-        />
-        {/* <!-- End Google Tag Manager --> */}
+        ></script>
       </head>
+
       <body className={montserrat.className}>
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-NV4SSRH7"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         <LpPaintNavbar />
         {children}
         <LpPaintFooter />
