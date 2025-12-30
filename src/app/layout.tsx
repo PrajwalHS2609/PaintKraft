2d34a54802ba4ec41a7522c2e41d63ffee94f719
Prajwal H S <prajwalhs4india@gmail.com>
2025-12-30 11:08:01 +0530

    google code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,8 +40,41 @@ export default function RootLayout({
     `,
           }}
         />
+        {/* <!-- Google Tag Manager --> */}
+        {/* Google Ads Conversion */}
+        <Script
+          id="gtag-conversion"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              function gtag_report_conversion(url) {
+                var callback = function () {
+                  if (typeof url !== 'undefined') {
+                    window.location = url;
+                  }
+                };
+                gtag('event', 'conversion', {
+                  'send_to': 'AW-17441114317/vVPECIDiwc4bEM2RyfxA',
+                  'event_callback': callback
+                });
+                return false;
+              }
+            `,
+          }}
+        />
+        {/* <!-- End Google Tag Manager --> */}
       </head>
       <body className={montserrat.className}>
+        {/* <!-- Google Tag Manager (noscript) --> */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NV4SSRH7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+        {/* <!-- End Google Tag Manager (noscript) --> */}
         <LpPaintNavbar />
         {children}
         <LpPaintFooter />
