b9910ca22d18a56cd3e52231a0d920b8276da8e2
Prajwal H S <prajwalhs4india@gmail.com>
2026-01-17 17:54:55 +0530

    add paint shades

--- src/app/asian-paints/page.jsx
+++ src/app/asian-paints/page.jsx
@@ -4,8 +4,8 @@ import ServiceHeroSection from "../../components/ServiceComponents/ServiceHeroSe
 export default function AsianPaints() {
   return (
     <div>
-      <ServiceHeroSection />
-      <AsianPaintShadesEssentials/>
+      <ServiceHeroSection title="Asian Paints Shade Card" description="Asian Paints Shades is one of the most loved shades in the Asian Paints palette. This page helps you visualize it in real homes, compare it with similar tones, and get expert tips for ideal usage." />
+      <AsianPaintShadesEssentials />
     </div>
   );
 }
