60195955d661032a0838368325c667e9e8a3fee6
Prajwal H S <prajwalhs4india@gmail.com>
2026-01-26 14:30:57 +0530

    shades changes

--- src/app/asian-shade/page.jsx
+++ src/app/asian-shade/page.jsx
@@ -1,10 +1,11 @@
 import AsianPaintShadesEssentials from "../../components/AsianPaint/AsianPaintShadesEssentials";
 import ServiceHeroSection from "../../components/ServiceComponents/ServiceHeroSection";
+import { bannerImg } from '@/images/LpBannerImg.png';
 
 export default function AsianShade() {
   return (
     <div>
-      <ServiceHeroSection title="Asian Paints Shade Card" description="Asian Paints Shades is one of the most loved shades in the Asian Paints palette. This page helps you visualize it in real homes, compare it with similar tones, and get expert tips for ideal usage." />
+      <ServiceHeroSection bannerImg="https://images.pexels.com/photos/5933/color-paint-palette-wall-painting.jpg" title="Asian Paints Shade Card" description="Asian Paints Shades is one of the most loved shades in the Asian Paints palette. This page helps you visualize it in real homes, compare it with similar tones, and get expert tips for ideal usage." />
       <AsianPaintShadesEssentials/>
     </div>
   );
