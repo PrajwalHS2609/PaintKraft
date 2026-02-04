f3534b8e19ba0912d0e103d4baee3cc9e9c3a17b
Prajwal H S <prajwalhs4india@gmail.com>
2026-02-04 16:52:18 +0530

    top bar changes

--- src/components/LpPaintNavbar/LpPaintTopbar.jsx
+++ src/components/LpPaintNavbar/LpPaintTopbar.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import "./LpPaintNavbar.css";
 import { FaWhatsapp } from "react-icons/fa6";
+import { IoMdCall } from "react-icons/io";
 const LpPaintTopbar = () => {
   return (
     <div className="lpPaintTop-container">
       <div className="lpPaintTop-content"></div>
       <div className="lpPaintTop-content">
-        <h6>Get a Home Painting Estimate on WhatsApp in Just 30 Minutes!</h6>
+        <h6>1-Hr Home Visit or Get 10% Off</h6>
       </div>
       <div className="lpPaintTop-content">
         <a href="https://api.whatsapp.com/send?phone=917090234446">
           <FaWhatsapp className="lpPaintTop-icon" />
+        </a>
+        <a href="tel:9071274446">
+          <IoMdCall className="lpPaintTop-icon" id="lpPaintTop-icon2"/>
         </a>
       </div>
     </div>
